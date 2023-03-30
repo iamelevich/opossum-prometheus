5fa54a2104782933b70575ddf973be5aa3d5f913
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-03-30 16:05:25 +0200

    docs: add some examples

---
+++ examples/custom-labels.ts
@@ -0,0 +1,24 @@
+import CircuitBreaker from 'opossum';
+import promClient from 'prom-client';
+import { CircuitBreakerMetrics } from '../src';
+
+(async () => {
+  const myCircuitBreaker = new CircuitBreaker(async () => 'my-data', {
+    name: 'my-circuit-breaker',
+  });
+
+  const myCircuitBreakerMetrics = new CircuitBreakerMetrics({
+    enabled: true,
+    client: promClient,
+    registry: promClient.register,
+    circuitBreakers: [myCircuitBreaker],
+    customLabels: {
+      service: 'my-service',
+      version: '1.0.0',
+    },
+  });
+
+  await myCircuitBreaker.fire();
+
+  console.log(await myCircuitBreakerMetrics.metrics);
+})();
