57be883c0a65c1207313688eb1fa8928cc67588c
Ilya Amelevich <ilya.amelevich@gmail.com>
2025-02-15 15:29:37 +0100

    style: remove xo and prettier and use biome instead
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- examples/overrides.ts
+++ examples/overrides.ts
@@ -3,34 +3,34 @@ import promClient from 'prom-client';
 import { CircuitBreakerMetrics } from '../src';
 
 (async () => {
-  const myCircuitBreaker = new CircuitBreaker(async () => 'my-data', {
-    name: 'my-circuit-breaker',
-  });
+	const myCircuitBreaker = new CircuitBreaker(async () => 'my-data', {
+		name: 'my-circuit-breaker',
+	});
 
-  const myCircuitBreakerMetrics = new CircuitBreakerMetrics({
-    enabled: true,
-    client: promClient,
-    registry: promClient.register,
-    circuitBreakers: [myCircuitBreaker],
-    overrides: {
-      counter: {
-        name: 'my_cb_counter',
-        labels: {
-          name: 'cb_name',
-          event: 'cb_event',
-        },
-      },
-      summary: {
-        name: 'my_cb_summary',
-        labels: {
-          name: 'cb_name',
-          event: 'cb_event',
-        },
-      },
-    },
-  });
+	const myCircuitBreakerMetrics = new CircuitBreakerMetrics({
+		enabled: true,
+		client: promClient,
+		registry: promClient.register,
+		circuitBreakers: [myCircuitBreaker],
+		overrides: {
+			counter: {
+				name: 'my_cb_counter',
+				labels: {
+					name: 'cb_name',
+					event: 'cb_event',
+				},
+			},
+			summary: {
+				name: 'my_cb_summary',
+				labels: {
+					name: 'cb_name',
+					event: 'cb_event',
+				},
+			},
+		},
+	});
 
-  await myCircuitBreaker.fire();
+	await myCircuitBreaker.fire();
 
-  console.log(await myCircuitBreakerMetrics.metrics);
+	console.log(await myCircuitBreakerMetrics.metrics);
 })();
